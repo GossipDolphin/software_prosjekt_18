18074a1a7b770f1472c0fb2b048b64409efba385
GossipDolphin <mmollevik@hotmail.com>
2019-10-11 12:27:06 +0200

    endret konstruktører som tar imot seg selv til "this"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
         LocalDateTime randomtid = LocalDateTime.parse("2019-09-09 10:30", formatter);
 
 
-        Organiser Ola = new Organiser("Ola@hotmail.com", "admin", "Ola", "Normann");
+        Organiser Ola = new Organiser("Ola@hotmail.com", "admin", "Ola Normann");
         Arrangement HelseArrangement = Ola.opprettArrangement("Helse arrangement","dankestrace", randomtid, randomtid, "Halden");
         Ola.leggTilArrangementIListen(HelseArrangement);
         Race race1 = new Race("ungdom 05 løp1" ,"raceracerace", randomtid, randomtid);
