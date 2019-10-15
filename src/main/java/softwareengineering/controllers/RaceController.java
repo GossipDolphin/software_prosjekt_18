58b4363fca6c34a445024ca7ee9e2b0e3f4f7f6e
GossipDolphin <mmollevik@hotmail.com>
2019-10-15 13:41:48 +0200

    controller for race

--- src/main/java/softwareengineering/controllers/RaceController.java
+++ src/main/java/softwareengineering/controllers/RaceController.java
@@ -1,5 +1,16 @@
 package softwareengineering.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RaceController {
 
+    @GetMapping("/race")
+        public String readCookie(@CookieValue(value = "name", defaultValue = "Atta") String username) {
+            return "Hey! My username is " + username;
+
+
+    }
 }
