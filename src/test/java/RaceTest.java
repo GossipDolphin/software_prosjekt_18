3d4664641aa32a70efd77306f308e9d751d6187a
TomorrowAvocado <thomas.bugge@hiof.no>
2019-10-22 11:56:38 +0200

    Laget test til getRaceByID.

--- src/test/java/RaceTest.java
+++ src/test/java/RaceTest.java
@@ -16,11 +16,11 @@ class RaceTest {
 
     private Organiser dummyOrganiser = new Organiser("post@tufte.no", "HeiaTufte", "Tufte IL");
     private Arrangement dummyarrangement = new Arrangement("Tuftes årlige joggekonkurranse", "Velkommen til vår årlige joggekonkurranse. Vi håper at dette blir året vi får en vinner som klarer å jogge frem til målstreken.", dummyTid, dummyTid, "Østensjøvannet", dummyOrganiser);
-    Race dummyRace = new Race("Joggekonkurranse", "Deltakerne jogger så fort og langt de kan i samme retning", dummyTid, dummyTid, dummyarrangement);
+    private Race dummyRace = new Race("Joggekonkurranse", "Deltakerne jogger så fort og langt de kan i samme retning", dummyTid, dummyTid, dummyarrangement);
 
     @Test
     void getRaceById() {
-
-
+        dummyRace.setId(666);
+        assertEquals(dummyRace, Race.getRaceById(666));
     }
 }
