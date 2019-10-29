f5c527bf4fa4e9f56a25197597a358006a3daff2
GossipDolphin <mmollevik@hotmail.com>
2019-10-29 13:21:58 +0100

    laget metode i main som kjører appen på lokalhost8080 når koden compileres og kjøres

--- src/main/java/softwareengineering/Main.java
+++ src/main/java/softwareengineering/Main.java
@@ -7,13 +7,14 @@ import softwareengineering.model.Deltager;
 import softwareengineering.model.Organiser;
 import softwareengineering.model.Race;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime randomtid = LocalDateTime.parse("2019-09-09 10:30", formatter);
 
@@ -34,5 +35,10 @@ public class Main {
         petter.joinRace(race1);
 
         SpringApplication.run(Main.class);
+        openHomePage();
+    }
+    private static void openHomePage() throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080");
     }
 }
