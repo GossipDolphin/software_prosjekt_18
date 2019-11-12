344cc631f36d5b844dba73ff84b273091f7a499e
GossipDolphin <mmollevik@hotmail.com>
2019-11-12 12:27:44 +0100

    laget knapper for å logge inn som dummy profiler i innlogging

--- src/main/java/softwareengineering/Main.java
+++ src/main/java/softwareengineering/Main.java
@@ -18,8 +18,8 @@ public class Main {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime randomtid = LocalDateTime.parse("2019-09-09 10:30", formatter);
 
-        Organiser Ola = new Organiser("Ola@hotmail.com", "admin", "Halden skiklubb");
-        Deltager petter = new Deltager("petter@gmail.com", "admin","Petter", "Petterson");
+        Organiser Ola = new Organiser("ola@gmail.com", "test1", "Halden skiklubb");
+        Deltager petter = new Deltager("petter@gmail.com", "test2","Petter", "Petterson");
         Arrangement HelseArrangement = new Arrangement("Helse arrangement","Løp til du blir ekstra sliten", randomtid, randomtid, "BRA Veien 6b, 1783 Halden", Ola);
         Arrangement skiLoep = new Arrangement("Halden Skirenn", "Langrenn",randomtid,randomtid,"Walkers gate 4, 1771 Halden", Ola);
         Race race1 = new Race("ungdom 05 løp1" ,"la ungdommen løpe!", randomtid, randomtid, HelseArrangement);
